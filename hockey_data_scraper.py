8405d95c0727cfd6ee2646ee3bd7cac3cb6132a2
Travis Mattson <travmatt7@gmail.com>
2019-11-25 19:45:52 -0600

    python scraper initial

---
+++ hockey_data_scraper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 18:38:13 2019
+
+@author: admin
+"""
+
+import os 
+import pandas as pd
+import re
+from bs4 import BeautifulSoup as bs
+
+#Go to directory
+os.chdir('/Users/admin/Blog_Data/Cawledge_Hawkey')
